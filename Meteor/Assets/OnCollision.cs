7f28477ef8f79510512abcae2ddea67024220ae5
Bella <Isabella.Cadoni@informatik.hs-fulda.de>
2018-08-16 13:10:12 +0200

    Meteor Projekt
    Das ganze Meteor Projekt

---
+++ Meteor/Assets/OnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour {
+
+    //Gibt auf der Konsole aus, wann Kollision anfängt
+
+	void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log("Enter called");
+	}
+
+    //
+
+	void OnCollisionStay(Collision collision)
+	{
+        Debug.Log("Stay occuring");
+	}
+
+    //
+
+	void OnCollisionExit(Collision collision)
+	{
+        Debug.Log("Exit called");
+	}
+
+}
