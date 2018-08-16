135c22e5882f00a29ea35e5cb361029f6b02a057
tülay özcan <tulayozcan@tulays-MacBook-Pro.local>
2018-08-16 14:25:41 +0200

    PLANET
    Die Welt mit Faux Gravity und der Player der sich bewegt

---
+++ Planet faux gravity/Assets/Skripte/CameraSwitch.cs
@@ -0,0 +1,31 @@
+﻿var FPSCam : Camera;
+var TPCam : Camera;
+
+private var CamSwitch = false;
+
+function Start()
+{
+    FPSCam.GetComponent(Camera).enabled = true;
+    TPCam.GetComponent(Camera).enabled = false;
+}
+
+function Update()
+{
+
+    if (Input.GetKeyDown(KeyCode.1))
+    {
+        CamSwitch = !CamSwitch;
+    }
+
+    if (Camera == true)
+    {
+        FPSCam.GetComponent(Camera).enabled = false;
+        TPCam.GetComponent(Camera).enabled = true;
+    }
+
+    else
+    {
+        FPSCam.GetComponent(Camera).enabled = true;
+        TPCam.GetComponent(Camera).enabled = false;
+    }
+}
