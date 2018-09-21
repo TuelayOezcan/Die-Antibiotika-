7cb2a0b3b234844ee933d6ebb51e7e7acd16c09e
tülay özcan <tulayozcan@tulays-MBP.local>
2018-09-21 16:54:57 +0200

    Faux Gravity kommentiert
    Genauer beschrieben, was ich in den
    Skript gemacht habe

--- Planet faux gravity/Assets/Scenes/Script/FauxGravityAttractor.cs
+++ Planet faux gravity/Assets/Scenes/Script/FauxGravityAttractor.cs
@@ -12,13 +12,23 @@ public class FauxGravityAttractor : MonoBehaviour
     {
 
         Rigidbody rb = reciever.GetComponent<Rigidbody>();
-        Vector3 forceUp = reciever.position - transform.position;
+        //Rigidbody kraft zufügen die das ganze Objekt an den Planeten 
+        //herandrückt
+
+        //Zeigt von dem Planeten zur Transformkomponente des Resivers
+        Vector3 forceUp = reciever.position - transform.position; 
         Vector3 dir;
+        //egal wie groß die Kraft tatsächlich ist von forceUp, wir normalisieren 
+        //sie auf eine Vectorlänge von 1 damit haben wir wirklich nur noch
+        //eine Richtung für den Vector für diese Richtung und die tatsächliche
+        //höhe für dieses Vectors wird durch gravityForce (-10) bestimmt.
         dir = gravityForce * forceUp.normalized;
 
         rb.AddForce(dir);
+        //Wo zeigt aktuell seine Y-Achse hin 
         Vector3 reciverUp = reciever.up;
 
+        //Rotation bestimmen die dafür sorgt, das er Richtig auf dem Planeten draufsteht
         Quaternion rot = Quaternion.FromToRotation(reciverUp, forceUp) * reciever.rotation;
 
         reciever.rotation = rot;
--- Planet faux gravity/Assets/Scenes/Script/FauxGravityBody.cs
+++ Planet faux gravity/Assets/Scenes/Script/FauxGravityBody.cs
@@ -10,14 +10,21 @@ public class FauxGravityBody : MonoBehaviour
     void Start()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
+
+        //useGravity deaktivieren, weil ich ja die Gravitationskraft selber erzeugen
         rb.useGravity = false;
+        //Rotation einfriehren schließlich mache ich das ja auch in der ApplyGravity Funktion 
         rb.freezeRotation = true;
 
     }
 
-    // Update is called once per frame
+    //FixedUpdate wird von der Physikangen gesteuert und immer in den gleihen Abständen 
+    //aufgerufen wird im gegensatz zum Update das Framebasiert funktuniert und weil ich eine
+    //ApplyGravity Funtion eine Physikalische Kraft im Rigidbody hinzufüge. Dadurch habe ich keine
+    //Probleme wenn sich die Framlänge mal ändern sollte.
     void FixedUpdate()
     {
+        //Rufe die ApplyGravity Funktion auf und geben unser eigenes Transform dieser Funktion 
         gravity.ApplyGravity(transform);
 
     }
