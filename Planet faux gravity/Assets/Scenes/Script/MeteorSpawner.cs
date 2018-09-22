3efe2da119ccdc746ef335b3cabcbfbdf724a33e
Bella <Isabella.Cadoni@informatik.hs-fulda.de>
2018-09-22 11:57:18 +0200

    (Nicht Wichtig) div. Veränderungen durch
    Unwichtig, Veränderungen durch das Öffnen einer anderen Datei

--- Planet faux gravity/Assets/Scenes/Script/MeteorSpawner.cs
+++ Planet faux gravity/Assets/Scenes/Script/MeteorSpawner.cs
@@ -47,8 +47,3 @@ public class MeteorSpawner : MonoBehaviour
     }
 }
 
-
-/*float SphericalDistance(Vector3 position1, Vector3 position2)
-{
-    return Mathf.Acos(Vector3.Dot(position1, position2));
-}*/
